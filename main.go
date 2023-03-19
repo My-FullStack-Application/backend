3c79d7cbe4019450e52f39d773367530d6406d02
GK <mdvv85009@gmail.com>
2023-03-19 15:19:13 +0800

    feature: add viper config tools support

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func heartbeat(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "OK",
+	ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+}
+
+func version(ctx *gin.Context) {
+	ctx.String(http.StatusOK, viper.GetString("server.version"))
+}
+
+func LoadConfiguration() {
+	viper.SetConfigFile("local.yaml")
+	viper.SetConfigName("local")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configuration")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("config file failed to load: %s", err))
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file has been changed:", e.Name)
 	})
 }
 
 func main() {
+	LoadConfiguration()
+
 	server := gin.Default()
 	server.GET("/heartbeat", heartbeat)
-	server.Run("localhost:8080")
+	server.GET("/version", version)
+
+	address := viper.GetString("server.host.address")
+	port := viper.GetInt("server.host.port")
+	server.Run(fmt.Sprintf("%s:%d", address, port))
 }
